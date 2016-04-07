36c6757aac8ff1af1e301167deb70fa47d95135d
Sandra <eloyin@ubuntu.(none)>
2016-04-07 22:52:27 +0200

    Primer intento, sin login en annotated

---
+++ myproject/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'myproject.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^createPage/$', 'cms_put.views.createPage'),
+    url(r'^mod/$', 'cms_put.views.managePages'),
+    url(r'^accounts/profile/$', 'cms_put.views.redirectHome'),
+    url(r'^login$','django.contrib.auth.views.login'),
+    url(r'^logout$','django.contrib.auth.views.logout'),
+    url(r'^$', 'cms_put.views.homePage'),
+    url(r'^home/$', 'cms_put.views.redirectHome'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^annotated/(.*)', 'cms_put.views.pickPageTemplate'),
+    url(r'(.*)/', 'cms_put.views.pickPage'),
+)
